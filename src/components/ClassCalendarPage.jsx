e868b2ab9dae2d131ea41a5478198c00202ce44d
Student <example@example.com>
2019-12-12 15:49:47 -0800

    Add tables for class calendar and client display

--- src/components/ClassCalendarPage.jsx
+++ src/components/ClassCalendarPage.jsx
@@ -1,45 +1,71 @@
 import React from 'react'
-import { Table } from 'semantic-ui-react'
+import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 
-const TableExampleCelled = () => (
+const ClassCalendarPage = () => (
   <Table celled>
     <Table.Header>
       <Table.Row>
-        <Table.HeaderCell>Name</Table.HeaderCell>
-        <Table.HeaderCell>Status</Table.HeaderCell>
-        <Table.HeaderCell>Notes</Table.HeaderCell>
+        <Table.HeaderCell>Date</Table.HeaderCell>
+        <Table.HeaderCell>Sign-In</Table.HeaderCell>
+        <Table.HeaderCell>Time</Table.HeaderCell>
+        <Table.HeaderCell>Class</Table.HeaderCell>
+        <Table.HeaderCell>Instructor</Table.HeaderCell>
+        <Table.HeaderCell>Location</Table.HeaderCell>
       </Table.Row>
     </Table.Header>
 
     <Table.Body>
       <Table.Row>
-        <Table.Cell>John</Table.Cell>
-        <Table.Cell>Approved</Table.Cell>
-        <Table.Cell>None</Table.Cell>
+        <Table.Cell>December  12, 2019</Table.Cell>
+        <Table.Cell>Link to ClientDisplayPage</Table.Cell>
+        <Table.Cell>9am-10am</Table.Cell>
+        <Table.Cell>Deep Flow</Table.Cell>
+        <Table.Cell>Emma Stern</Table.Cell>
+        <Table.Cell>MOD on 45th</Table.Cell>
       </Table.Row>
       <Table.Row>
-        <Table.Cell>Jamie</Table.Cell>
-        <Table.Cell>Approved</Table.Cell>
-        <Table.Cell>Requires call</Table.Cell>
+        <Table.Cell>December  12, 2019</Table.Cell>
+      <Table.Cell>Link to ClientDisplayPage</Table.Cell>
+        <Table.Cell>9am-10am</Table.Cell>
+        <Table.Cell>Deep Flow</Table.Cell>
+        <Table.Cell>Emma Stern</Table.Cell>
+        <Table.Cell>MOD on 45th</Table.Cell>
       </Table.Row>
       <Table.Row>
-        <Table.Cell>Jill</Table.Cell>
-        <Table.Cell>Denied</Table.Cell>
-        <Table.Cell>None</Table.Cell>
+        <Table.Cell>December  12, 2019</Table.Cell>
+        <Table.Cell>Link to ClientDisplayPage</Table.Cell>
+        <Table.Cell>9am-10am</Table.Cell>
+        <Table.Cell>Deep Flow</Table.Cell>
+        <Table.Cell>Emma Stern</Table.Cell>
+        <Table.Cell>MOD on 45th</Table.Cell>
       </Table.Row>
     </Table.Body>
 
     <Table.Footer>
       <Table.Row>
-        <Table.HeaderCell>3 People</Table.HeaderCell>
-        <Table.HeaderCell>2 Approved</Table.HeaderCell>
-        <Table.HeaderCell />
+        <Table.HeaderCell colSpan='3'>
+          <Menu pagination>
+            <Menu.Item as='a' icon>
+              <Icon name='chevron left' />
+            </Menu.Item>
+            <Menu.Item as='a'>1</Menu.Item>
+            <Menu.Item as='a'>2</Menu.Item>
+            <Menu.Item as='a'>3</Menu.Item>
+            <Menu.Item as='a'>4</Menu.Item>
+            <Menu.Item as='a' icon>
+              <Icon name='chevron right' />
+            </Menu.Item>
+          </Menu>
+        </Table.HeaderCell>
       </Table.Row>
     </Table.Footer>
   </Table>
 )
 
-export default TableExampleCelled
+export default ClassCalendarPage;
+
+
+// https://react.semantic-ui.com/collections/table/
 
 
 
@@ -47,20 +73,3 @@ export default TableExampleCelled
 
 
 
-
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-
-
-// function ClassCalendarPage() {
-// 	return(
-// 		<div>
-
-			
-// 		</div>
-// 	);
-// }
-
-// export default ClassCalendarPage;
--- src/components/ClientDisplayPage.jsx
+++ src/components/ClientDisplayPage.jsx
@@ -10,10 +10,67 @@ class ClientDisplayPage extends React.Component {
   }
   render() {
     return(
-      <div>
-        <p>NICE</p>
-        <ClientSearchBar /> 
-      </div>
+      <div className="clientDisplay"> 
+        <div className='clientSearch'>
+          <ClientSearchBar /> 
+        </div> 
+        <div className='clientTable'>
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Name</Table.HeaderCell>
+                <Table.HeaderCell>Email</Table.HeaderCell>
+                <Table.HeaderCell>Phone</Table.HeaderCell>
+                <Table.HeaderCell>Signed In</Table.HeaderCell>
+                <Table.HeaderCell>Purchase</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>Lauren Musante</Table.Cell>
+                <Table.Cell>Lauren@mail.com</Table.Cell>
+                <Table.Cell>1234567890</Table.Cell>
+                <Table.Cell>Checkbox here</Table.Cell>
+                <Table.Cell>BUY button here</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+              <Table.Cell>Lauren Musante</Table.Cell>
+                <Table.Cell>Lauren@mail.com</Table.Cell>
+                <Table.Cell>1234567890</Table.Cell>
+                <Table.Cell>Checkbox here</Table.Cell>
+                <Table.Cell>BUY button here</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+              <Table.Cell>Lauren Musante</Table.Cell>
+                <Table.Cell>Lauren@mail.com</Table.Cell>
+                <Table.Cell>1234567890</Table.Cell>
+                <Table.Cell>Checkbox here</Table.Cell>
+                <Table.Cell>BUY button here</Table.Cell>
+              </Table.Row>
+            </Table.Body>
+
+            <Table.Footer>
+              <Table.Row>
+                <Table.HeaderCell colSpan='3'>
+                  <Menu floated='right' pagination>
+                    <Menu.Item as='a' icon>
+                      <Icon name='chevron left' />
+                    </Menu.Item>
+                    <Menu.Item as='a'>1</Menu.Item>
+                    <Menu.Item as='a'>2</Menu.Item>
+                    <Menu.Item as='a'>3</Menu.Item>
+                    <Menu.Item as='a'>4</Menu.Item>
+                    <Menu.Item as='a' icon>
+                      <Icon name='chevron right' />
+                    </Menu.Item>
+                  </Menu>
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Footer>
+          </Table>
+        </div> 
+      </div> //clientDisplay
     )
   }
 } 
